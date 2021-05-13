471cd20314a3cfb6709c058a613fa07b91964ffb
Chris Noldus <chris@noldus.geek.nz>
2021-05-14 07:18:31 +1200

    tidy up

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# pword-regex-exp
-A place to experiment with various password validation settings
+# Password Tester
+A place to experiment with various password validation settings. Easily hosted on github pages or any spare webserver. 
 
-Open index.html in a browser and try different passwords.
+Open index.html in a browser and try different passwords to get a feel for how the current settings will affect the strength meter.
+
+Strength caclulation is done in password_checker.js. Try changing some of the default level values to see what impact it has.
 
 Copyright Chris Noldus 2021
 
--- password_checker.js
+++ password_checker.js
@@ -1,50 +1,51 @@
 //DEFAULT VALUES
-
-var default_suggest="Use 8 or more characters with a mix of letters, numbers and symbols";
+var min_length = 8; //Even if the checker thinks the password is long enough, it will be rejected if its shorter than this
+var default_suggest = "Use 8 or more characters with a mix of letters, numbers and symbols";
 var levels = [ 
   {
     "text" : "Too weak",
     "class" : "too-weak",
-  }, 
-  {
+  }, {
    "text" : "Weak",
     "class" : "weak",
     "guesses" : 10000
-  },  
-  {
-    "text" : "could be stronger",
+  }, {
+    "text" : "Almost there...",
     "class" : "average",
     "guesses" : 1000000
-  }, 
-  {
+  }, {
     "text" : "Strong",
     "class" : "strong",
     "guesses" : 100000000
   }, 
- ];
+];
 
 // Check the strength of the current input using zxcvbn
 function checkStrength(event) {
+  // Initialise
   var klass = levels[0];
   var bar = document.querySelector('#bar');
   var pass = document.querySelector('#pass').value;
 
+  // password analysis
   var result = zxcvbn(pass);
   var guesses = result.guesses;
   var suggestions = result.feedback.suggestions;
 
+  // Calculate what level the password is at
   var level = 0;
   for(var i =1; pass.length && i<levels.length; i++) 
     if (levels[i].guesses < guesses) level=i
 
-  if (pass.length < 8 && level >= 3) level = 2;
-
+  if (pass.length < min_length && level >= 3) level = 2; // If password < min_length, it cant pass
   klass = guesses >= levels[level].guesses ? levels[level] : klass
 
+  // Update UI
   document.querySelector('#bar').className = klass.class;
   document.querySelector('#str-text').className = klass.class+"-text";
   document.querySelector('#str-text').innerHTML = klass.text;
   
+  // Insert suggestion
   var suggest = default_suggest;
   if (level == 3) suggest = "";
   document.querySelector("#suggest").innerHTML = suggest;
